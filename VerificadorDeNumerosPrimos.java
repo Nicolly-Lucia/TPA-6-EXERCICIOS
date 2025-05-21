7e38cc52c4f04800d02f3f25b0d1ae920475fdb4
Nicolly Lucia Da Silva <nicklucia0401@gmail.com>
2025-05-21 14:45:39 -0300

    Add files via upload

---
+++ VerificadorDeNumerosPrimos.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.verificadordenumerosprimos;
+
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+import java.util.Scanner;
+
+public class VerificadorDeNumerosPrimos {
+
+    public static void main(String[] args) {
+ Scanner scanner = new Scanner(System.in);
+        int opcao;
+        
+        do {
+            System.out.println("\nVERIFICADOR DE NÚMERO PRIMO");
+            System.out.println("--------------------------");
+            System.out.println("1 - Verificar se um número é primo");
+            System.out.println("0 - Sair");
+            System.out.print("Escolha uma opção: ");
+            
+            opcao = scanner.nextInt();
+            
+            if (opcao == 1) {
+                System.out.print("Digite um número: ");
+                int numero = scanner.nextInt();
+                
+                boolean Primo = verificarPrimo(numero);
+                
+                if (Primo) {
+                    System.out.println(numero + " é um número primo.");
+                } else {
+                    System.out.println(numero + " não é um número primo.");
+                }
+            } else if (opcao != 0) {
+                System.out.println("Opção inválida!");
+            }
+            
+        } while (opcao != 0);
+        
+        System.out.println("Programa encerrado.");
+        scanner.close();
+    }
+    
+    // Método para verificar se um número é primo
+    public static boolean verificarPrimo(int numero) {
+        if (numero <= 1) {
+            return false;
+        }
+        
+        for (int i = 2; i <= Math.sqrt(numero); i++) {
+            if (numero % i == 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
